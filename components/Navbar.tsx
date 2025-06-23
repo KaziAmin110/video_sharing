6ba5ebdccc72a1a7ae82034ee58011af71e85cf9
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-06-22 22:29:22 -0400

    Added Header Components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const user = {};
 const Navbar = () => {
+  const router = useRouter();
   return (
     <header className="navbar">
       <nav>
@@ -13,7 +17,7 @@ const Navbar = () => {
 
         {user && (
           <figure>
-            <button>
+            <button onClick={() => router.push("/profile/123456")}>
               <Image src="/assets/images/dummy.jpg" alt="user" width={36} height={36} className="rounded-full aspect-square"/>
             </button>
             <button className="cursor-pointer">
