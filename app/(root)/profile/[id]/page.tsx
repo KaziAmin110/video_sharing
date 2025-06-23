bb40079c540859ee563d189dd6a67832d0b21852
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-06-23 19:18:10 -0400

    Added Dynamic User Name to Profile Page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,15 +1,18 @@
 import Header from "@/components/Header";
 
-const Page = async ({params} : ParamsWithSearch) => {
-    const {id} = await params;
+const Page = async ({ params }: ParamsWithSearch) => {
+  const userName = "Kazi Amin";
+  const { id } = await params;
   return (
     <div className="wrapper page">
-        <Header subHeader="adrian@jsmastery.pro" title="Kazi Amin" userImg="/assets/images/dummy.jpg" />
-        <h1 className='text-2xl font-karla'>
-            USER ID: {id}
-        </h1>
+      <Header
+        subHeader="adrian@jsmastery.pro"
+        title={userName}
+        userImg="/assets/images/dummy.jpg"
+      />
+      <h1 className="text-2xl font-karla">USER ID: {id}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
